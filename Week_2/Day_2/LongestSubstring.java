fc28727ec7f1008c3d5426295fa20541b26f9692
Ananya Singh <ananya.suhani9415@gmail.com>
2026-06-09 22:40:48 +0530

    Add files via upload

---
+++ Week_2/Day_2/LongestSubstring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        int maxLen=Integer.MIN_VALUE;
+        int left=0;
+        int right=0;
+        HashSet<Character>set=new HashSet<>();
+        if(n==0)
+        return 0;
+        while(right<n)
+        {
+            if(!set.contains(s.charAt(right)))
+            {
+                set.add(s.charAt(right));
+                maxLen=Math.max(maxLen,right-left+1);
+                right++;
+            }
+            else
+            {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+        
+    }
+}
